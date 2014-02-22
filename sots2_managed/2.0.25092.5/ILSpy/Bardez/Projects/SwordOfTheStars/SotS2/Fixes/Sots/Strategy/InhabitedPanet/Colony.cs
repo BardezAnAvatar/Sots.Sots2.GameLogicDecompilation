9279f63531bbd4d5feea632ba95e7c5d0b36100d
Erik "Bardez" Frantz <bardez@bardez.net>
2014-02-22 03:35:45 +0000

    Added a GetTaxRevenue method that accepts a collection of colonies
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Sword of the Stars;C962

--- sots2_managed/2.0.25092.5/ILSpy/Bardez/Projects/SwordOfTheStars/SotS2/Fixes/Sots/Strategy/InhabitedPanet/Colony.cs
+++ sots2_managed/2.0.25092.5/ILSpy/Bardez/Projects/SwordOfTheStars/SotS2/Fixes/Sots/Strategy/InhabitedPanet/Colony.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Kerberos.Sots;
 using Kerberos.Sots.Data;
 using Kerberos.Sots.StarFleet;
 using Kerberos.Sots.Steam;
@@ -8,8 +9,8 @@ using Source = Kerberos.Sots.Strategy.InhabitedPlanet;
 
 namespace Bardez.Projects.SwordOfTheStars.SotS2.Fixes.Sots.Strategy.InhabitedPanet
 {
-    /// <summary>This class contains code fixes to general SotS2 logic in Kerberos.Sots.Strategy.InhabitedPanet.Colony</summary>
-	public class Colony
+    /// <summary>This class contains performance enhancements to general SotS2 logic in Kerberos.Sots.Strategy.InhabitedPanet.Colony</summary>
+	public static class Colony
     {
         /// <summary>Summarizes the empire-wide support cost of colonize without the recursive Linq summarizing that hits the DB redundantly</summary>
         /// <param name="db">GameDatabase to query</param>
@@ -65,6 +66,72 @@ namespace Bardez.Projects.SwordOfTheStars.SotS2.Fixes.Sots.Strategy.InhabitedPan
             }
 
             return supportCosts;
+        }
+
+        /// <summary>Gets the tax revenue for a collection of ColonyInfo colonies</summary>
+        /// <param name="game">App to query</param>
+        /// <param name="player">Owning player to search on and apply info for</param>
+        /// <param name="colony">Colony whose tax revenue is being calculated</param>
+        /// <returns>The tax revenue for the specified colonies</returns>
+        internal static Double GetTaxRevenue(App game, PlayerInfo player, IEnumerable<ColonyInfo> colonies)
+        {
+            Double taxRevenue = 0.0;
+            Double taxRate = (player.RateTax != 0f) ? (player.RateTax * 100f / game.AssetDatabase.TaxDivider) : 0f;
+            Boolean playerRaceHasSlaves = game.AssetDatabase.GetFaction(player.FactionID).HasSlaves();
+
+            //pre-collect strat modifiers, outside of the loop
+            Single alienCivilianTaxRate = game.GameDatabase.GetStratModifierFloatToApply(StratModifiers.AlienCivilianTaxRate, player.ID);
+            Single aiTaxBonuses = game.GameDatabase.GetStratModifierFloatToApply(StratModifiers.AIRevenueBonus, player.ID) * game.GameDatabase.GetStratModifierFloatToApply(StratModifiers.AIBenefitBonus, player.ID);
+            Single taxRevenueModifier = game.GameDatabase.GetStratModifierFloatToApply(StratModifiers.TaxRevenue, player.ID) - 1f;
+            Single difficultyTaxRevenueBonus = game.AssetDatabase.GetAIModifier(game, DifficultyModifiers.RevenueBonus, player.ID);
+
+            //calculate this above so that it can be applied for each colony. I would prefer to factor this out, but rebellions disallow that.
+            Single consolidatedBonuses = aiTaxBonuses + taxRevenueModifier + difficultyTaxRevenueBonus;
+
+            //This change might be considered possibly dangerous, though I doubt it.
+            //The homeworld for protectorates doesn't exist, and that should be the only case of player colony combinations for tax calculations.
+            //HomeworldInfo playerHomeworld = game.GameDatabase.GetPlayerHomeworld(colony.PlayerID);
+            HomeworldInfo playerHomeworld = game.GameDatabase.GetPlayerHomeworld(player.ID);
+
+            foreach (ColonyInfo colony in colonies)
+            {
+                Double colonyTaxRevenue = 0.0;
+
+                if (colony.RebellionType != RebellionType.None) //if colony is rebelling
+                    continue;
+
+                colonyTaxRevenue = colony.ImperialPop * taxRate;
+                ColonyFactionInfo[] factions = colony.Factions;
+                for (Int32 i = 0; i < factions.Length; i++)
+                {
+                    ColonyFactionInfo colonyFactionInfo = factions[i];
+                    if (!playerRaceHasSlaves || colonyFactionInfo.FactionID == player.FactionID)
+                    {
+                        Double civillianTax = colonyFactionInfo.CivilianPop * taxRate * 2.0;
+                        if (colonyFactionInfo.FactionID != player.FactionID)
+                            civillianTax *= alienCivilianTaxRate;
+
+                        colonyTaxRevenue += civillianTax;
+                    }
+                }
+
+                //Here the AI, tax modifier and difficulty tax bonus are pulled. I precomputed this.
+                colonyTaxRevenue += consolidatedBonuses;
+
+                if (!game.GameDatabase.GetStarSystemInfo(colony.CachedStarSystemID).IsOpen)
+                {
+                    colonyTaxRevenue *= 0.89999997615814209;    //Floating point error? I assume this is *= 0.9
+                }
+
+                //pulled the homeworld retrieval out to alleviate a DB call.
+
+                if (playerHomeworld != null && playerHomeworld.ColonyID == colony.ID)
+                    colonyTaxRevenue *= game.AssetDatabase.HomeworldTaxBonusMod;
+
+                taxRevenue += colonyTaxRevenue;
+            }
+
+            return taxRevenue;
         }
 	}
 }
